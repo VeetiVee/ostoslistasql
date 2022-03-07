ea22a88dc4e53e540cc5eca8939e24e048b9fd4e
VeetiVee <veeti.vainikka@gmail.com>
2022-03-07 20:26:02 +0200

    tehtävä tehty

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('coursedb.db');
 
 export default function App() {
+  const [product, setProduct] = useState('');
+  const [amount, setAmount] = useState('');
+  const [shoppingList, setShoppingList] = useState([]);
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists shoppingList (id integer primary key not null, amount text, product text);');
+    }, null, updateList); 
+  }, []);
+
+  // Save course
+  const saveItem = () => {
+    db.transaction(tx => {
+        tx.executeSql('insert into shoppingList (amount, product) values (?, ?);', [amount, product]);    
+      }, null, updateList
+    )
+  }
+
+  // Update courselist
+  const updateList = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from shoppingList;', [], (_, { rows }) =>
+        setShoppingList(rows._array)
+      ); 
+    }, null, null);
+  }
+
+  // Delete course
+  const deleteItem = (id) => {
+    db.transaction(
+      tx => {
+        tx.executeSql(`delete from shoppingList where id = ?;`, [id]);
+      }, null, updateList
+    )    
+  }
+
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 5,
+          width: "80%",
+          backgroundColor: "#fff",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder='Product' style={{marginTop: 30, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(product) => setProduct(product)}
+        value={product}/>  
+      <TextInput placeholder='Amount' style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(amount) => setAmount(amount)}
+        value={amount}/>      
+      <Button onPress={saveItem} title="Save" /> 
+      <Text style={{marginTop: 30, fontSize: 20}}>Shoppinglist</Text>
+      <FlatList 
+        style={{marginLeft : "5%"}}
+        keyExtractor={item => item.id.toString()} 
+        renderItem={({item}) => <View style={styles.listcontainer}><Text style={{fontSize: 18}}>{item.product}, {item.amount}</Text>
+        <Text style={{fontSize: 18, color: '#0000ff'}} onPress={() => deleteItem(item.id)}> Bought</Text></View>} 
+        data={shoppingList} 
+        ItemSeparatorComponent={listSeparator} 
+      />      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+ },
+ listcontainer: {
+  flexDirection: 'row',
+  backgroundColor: '#fff',
+  alignItems: 'center'
+ },
 });
